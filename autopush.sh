9487e6f059485069835d32de816a41e763c5fcb8
Tim Black <tim@alwaysreformed.com>
2012-04-06 02:22:44 -0500

    Initial import

---
+++ autopush.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+. ../bin/activate
+# TODO: If I start nosetests from this file too, and run autopush in the background, I can use this example to kill the autopush process before killing this process:  http://hacktux.com/bash/control/c
+# TODO: Here is how to start the forked process and kill it afterward:  http://hacktux.com/bash/ampersand
+#couchapp autopush http://local:local@localhost:45773/rcl
+# TODO: The watchdog command is like this:  watchmedo shell-command --patterns="*.py;*.txt" --recursive --command='nosetests' .
+couchapp browse . desktopcouch://rcl
+couchapp autopush --update-delay 1 desktopcouch://rcl
---
+++ shows/cong.js
@@ -0,0 +1,7 @@
+function(doc, req) {  
+	// this displays at http://127.0.0.1:53210/rcl/_design/rcl/_show/cong
+	return "Hello World!  This is an example congregation.";
+}
+// TODO: Move .couchapprc to ~/.couchapp.conf
+// TODO: One way to do versioning in CouchDB:  http://blog.couchbase.com/simple-document-versioning-couchdb
+
