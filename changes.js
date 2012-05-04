81a241fcc0b6f792f6e54bb88aafb85f4392fc43
Tim Black <tim@alwaysreformed.com>
2012-05-04 04:07:31 -0500

    Fix problems in changes listener monitor

--- changes.js
+++ changes.js
@@ -3,6 +3,8 @@
 // Call this file like this:
 // node http://localhost:41002/rcl/_design/changes
 
+// TODO:  When called from the commandline, this throws:
+// Error: Cannot find module 'cradle'
 var cradle = require('cradle'),
 	child = require('child_process'),
 	path = require('path'),
@@ -13,26 +15,35 @@ var cradle = require('cradle'),
 //TODO: Get port from command line args
 var db = new(cradle.Connection)('http://localhost', 60434).database('rcl');
 var feed = db.changes();
+var changes_listeners_filename = './changes_listeners_temp.js';
+
+function start_child_process(){
+	// Write to the temp file if it doesn't exist yet
+	if (!path.existsSync(changes_listeners_filename)){
+		db.get('_design/rcl', function(err, doc){
+			fs.writeFileSync(changes_listeners_filename, doc.changes_listeners);
+		});
+	}
+	// Spawn changes listener process
+	p = child.spawn(process.execPath, [changes_listeners_filename]);
+	// Log errors to stderr
+	p.stderr.on("data", function (chunk) {sys.error(chunk.toString());});
+}
+start_child_process();
 
 feed.on('change', function (change) {
-	// TODO: Remove this log call
-	console.log(change);
 	db.get(change.id, function(err, doc){
 		if (change.id && change.id.slice(0, '_design/'.length) === '_design/') {
 			// If the rcl design document changed, then reload the changes listeners.
 			console.log('A design document changed');
 			// Get the new design doc
-			if (doc.changes && listeners[doc._id]) {
+			if (doc.changes) {
 				// take down the process with the old design doc
 				p.kill();
-				// TODO: Consider renaming this file and its db equivalent to be more easily understood
-				var changes_listeners_filename = './changes_listeners_temp.js';
 				fs.unlinkSync(changes_listeners_filename);
 				// start up the process with the new design doc
 				fs.writeFileSync(changes_listeners_filename, doc.changes_listeners);
-				p = child.spawn(process.execPath, [changes_listeners_filename]);
-				// Log errors to stderr
-				p.stderr.on("data", function (chunk) {sys.error(chunk.toString());});
+				start_child_process();
 			}
 		} else {
 			// Feed the new doc into the changes listeners
@@ -40,7 +51,4 @@ feed.on('change', function (change) {
 			p.stdin.write(JSON.stringify(["ddoc", doc])+'\n');
 		}
 	})
-	// TODO: Handle all other changes
-	//console.log(change);
-	// TODO: Watch for requests to get the contents of a URL
 });
--- changes_listeners.js
+++ changes_listeners.js
@@ -5,13 +5,16 @@ stdin.setEncoding('utf8');
 var buffer = '';
 
 stdin.on('data', function (chunk) {
-  buffer += chunk.toString();
-  while (buffer.indexOf('\n') !== -1) {
-    line = buffer.slice(0, buffer.indexOf('\n'));
-    buffer = buffer.slice(buffer.indexOf('\n') + 1);  
-    var obj = JSON.parse(line);
-    if (obj[0] === "change") {
-      listener(obj[1]);
-    }
-  }
+	buffer += chunk.toString();
+	while (buffer.indexOf('\n') !== -1) {
+		line = buffer.slice(0, buffer.indexOf('\n'));
+		buffer = buffer.slice(buffer.indexOf('\n') + 1);  
+		var obj = JSON.parse(line);
+		if (obj[0] === "change") {
+			var doc = obj[1];
+			console.log(doc);
+			// Handle all other changes
+			// TODO: Watch for requests to get the contents of a URL
+		}
+	}
 });
