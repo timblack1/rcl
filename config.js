218f8caca63639c5f91d2793acf0af46452fab94
Tim Black <tim@alwaysreformed.com>
2012-07-23 16:59:03 -0500

    Maybe fixed Node changes listener loop

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 // Configure application-wide configuration settings here
 
-exports.debug = true; // turn log() on or off
+var fs = require('fs'),
+	get_db = require('./lib').get_db;
+
+// Export configuration settings
+exports.debug = false; // true or false; turns log() on or off
+exports.port = fs.readFileSync('./port.txt', 'ascii').trim();
+exports.auth = fs.readFileSync('./auth.txt', 'ascii').trim();
+exports.db = get_db(exports.port);
--- get_port.sh
+++ get_port.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-# Get the randomly-generated port for desktopcouch
-PORT=$(dbus-send --session --dest=org.desktopcouch.CouchDB    --print-reply --type=method_call /    org.desktopcouch.CouchDB.getPort | sed -e 's/^.* //' | tail -1)
-echo $PORT
+# Get the currently-known port
+PORT=$(cat port.txt)
+# Get the admin username and password for couchdb
+AUTH=$(cat auth.txt)
+DB_RESPONSE=$(curl http://$AUTH@localhost:$PORT/)
+# Test to see if the currently-known port works
+if [ `expr match "$DB_RESPONSE" "{\"couchdb\":\"Welcome\",\"version\".*"` -gt 0 ] ;
+then
+    echo "We have the right port!  So we aren't modifying the filesystem."
+else
+    echo "We don't have the right port yet.  So we'll get it and write it to port.txt."
+    # Get the randomly-generated port for desktopcouch
+    PORT=$(dbus-send --session --dest=org.desktopcouch.CouchDB    --print-reply --type=method_call /    org.desktopcouch.CouchDB.getPort | sed -e 's/^.* //' | tail -1)
+    echo $PORT > port.txt
+fi
