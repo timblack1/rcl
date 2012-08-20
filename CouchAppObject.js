287e211f6f80eacfad571c36fbab81f30732f472
Tim Black <tim@alwaysreformed.com>
2012-08-20 13:46:03 -0500

    Fix require() errors in couchapp, continue developing all aspects of CouchAppObject

--- CouchAppObject.js
+++ CouchAppObject.js
@@ -37,48 +37,43 @@
 //	the this[] array for new items.
 
 // db.js contains:
+// ---------------
 // var path = unescape(document.location.pathname).split('/'),
 //     db = $.couch.db(path[1]);
 // 
 // exports.db = db
+// ---------------
 // TODO: Fix db.js to run on server too, getting db name from config.js?
 
-// Use Node's require() function if we are running this in Node
-if (require=='undefined'){
-	// We're running in the browser (under Evently) rather than on the server in Node
-	var require = $$(this).app.require
-}
 //Define database access variable
-var db = require('db').db,
-	config = require('config')
+var config = require('config'),
+	db = config.db
 
 // TODO: Make it so the values returned by the object are returned synchronously, or the object
-//	takes a callback function on success.  I prefer making the object return synchronously, and
-//	hiding its asynchronous nature inside the object, but perhaps this is unwise, since it might
-//	block the UI.
+//	takes a callback function on success.  Initially, I prefer making the object return 
+//	synchronously, and hiding its asynchronous nature inside the object, but perhaps this is 
+//	unwise, since it might block the UI.
 
-// Base object
+// Base Base object
 var Base = {
     sub: function(){
         // Return a sub-instance of this object, like a subclass (technically, return an object
         // which has this object as its prototype.) We're using prototypal inheritance to save on
-    	// memory usage.
+    	// memory usage, following http://www.adobe.com/devnet/html5/articles/javascript-object-creation.html
         return Object.create(this)
     }
 }
-// Base object
+// Base Type object
 var Type = Base.sub()
 $.extend(true, Type, {
-    // TODO: Is init actually called automatically when a new instance is created via 
-	//	Object.create() or extend?
     // Initialize the object
     init : function(params) {
-        // Create views if they are not yet defined
+        // Create views for this type (not instance) if they are not yet defined
         this.create_views()
 
-        // Get this object's data if an instance with this id exists in the database
+        // Get this type instance's data if an instance with this id exists in the database
         if (params._id !== undefined) {
-            db.view(this._default_view, {
+            db.view(this.default_view.name, {
                 keys : [params._id],
                 include_docs : true,
                 success : function(data) {
@@ -86,54 +81,77 @@ $.extend(true, Type, {
                     // TODO: Do I need the deep copy?
                     // TODO: Run migrations here if we are running on read
                     var doc = this.run_migrations(data.rows[0])
-                    $.extend(true, this, doc)
+                    this.copy_to_save = $.extend(true, this, doc)
                 }
             })
         }
         // Else, populate this object
         else{
-            $.extend(true, this, params)
+        	this.copy_to_save = $.extend(true, this, params)
         }
         // TODO: Automate the save, like is done in
         //          https://github.com/bcoe/endtable/blob/master/lib/endtable-object.js and
         //          https://github.com/bcoe/endtable/blob/master/lib/monitored-object.js
         // TODO: Set up self-monitoring object code here
+        
 
         // TODO: Consider setting up a filtered changes listener that updates the local cache 
         //          of the object in memory if it changes in the database.  But will this cause
         //          conflicts in a problematic way? 
+        return this.copy_to_save
     },
     save:function(options){
         // Save the doc to the database
-        // TODO: Test to make sure this doesn't save this object's functions
         // TODO: Figure out how to handle the callback options object
         // TODO: Make sure to use the previous _rev when needed to guarantee that this 
         //  saves a new revision.
-        db.saveDoc(this, options);
+    	// TODO: Strip out the following attributes before saving:
+    	//	[views, relations, dirty, default_view]
+    	db.saveDoc(this, {
+    		success:function(data){
+    			this.dirty = false
+    			// TODO: Handle the response here.  Is data._rev the right reference?
+    			this._rev = data._rev
+    		},
+    		error:function(status){
+    			console.log(status)
+    		}
+    	});
     },
     // TODO: Consider mimicking EndTable's dynamic creation of views if they aren't 
     // identical to the ones defined in the CouchAppObject:  https://github.com/bcoe/endtable
     create_views:function(){
-        // TODO: Write code here!
+    	// TODO: Write code here!
     	// TODO: Will db writes initiated here overwrite or be overwritten by the design doc?
+    	//	Maybe I should have this put views into a separate CouchAppObject-specific design doc.
         // TODO: How can I keep this code from running every time a new object is created?  What if 
         //          the view already exists in the database?  It seems create_views needs to run 
         //          only at the time when the model is first loaded into the application (Or 
         //          maybe only at deployment.)  Does it make sense to run it when the browser first
         //          loads the application?  That seems like overkill.
-        // TODO: Create the default view if it doesn't exist yet
-        // Default view
-        this._default_view = function(parent) {
-        	// TODO: Maybe make this string different based on whether we're in cradle or 
-        	//	jquery.couch.js
-        	parent._default_view_name = 'db/' + parent.type
-            if (doc.type == this.type) {
-                emit(doc._id, null)
-            }
-        }
-        // TODO: How do I create this view in the database?  It should not be written to execute here,
-        //			but simply be declared here.  So does it need to be a string?  Can I get the string
-        //			programmatically?  Yes, with function.toString()
+
+    	// Declare default view, but only if it doesn't exist yet
+    	// TODO: It should not be written to execute here, but simply be declared here.  So does it need 
+    	//	to be a string?  Can I get the string programmatically?  Yes, with function.toString()
+    	if (typeof this.default_view !== 'undefined'){
+        	this.default_view = {
+        			name:'rcl/' + this.type,
+        			function_string:(
+        					function(){
+        						// TODO: On what key should this allow searching?  I assume _id.
+        						//	But because this makes it a "Get all" kind of view, maybe it 
+        						//	should be named accordingly (maybe views.all), and other similar 
+        						//	ORM query names should be borrowed from SQLAlchemy.
+        						// TODO: We could even create a default view on every object attribute,
+        						//	but that might waste disk space.
+        						if (doc.type == this.type) {
+        							emit(doc._id, null)
+        						}
+        					}).toString()
+        	}
+    	}
+    	// TODO: Create default views in database
+    	// TODO: How do I create this view in the database?
     	// TODO: Create relation views
 
         // TODO: Isn't it easier to simply get cgroup.congs.length() from the client side?
@@ -165,11 +183,15 @@ $.extend(true, Type, {
     }
 })
 
-// Base object
+// Base Model object
 var Model = Base.sub()
 $.extend(true, Model, {
 	// TODO: Set db changes listener somewhere that listens for db changes, and calls run_changes_handlers(doc)
 	//	Probably in this.init, which will need to be called after creating var model.
+	// Note that migrations are not absolutely necessary, because schema changes can be handled
+	//	almost completely by doing duck-typing schema detection in views and other code.  But if you
+	//	prefer not to have to worry about schema detection in the "other code," then you can write
+	//	migrations to be run here.
 	run_migrations:function(doc, mode){
         // See example in Python at http://stackoverflow.com/questions/130092/couchdb-document-model-changes/410840#410840
         // TODO: Decide whether to store migration_version in every object and run this code on 
--- evently/download/get_church_dir_from_url.js
+++ evently/download/get_church_dir_from_url.js
@@ -1,10 +1,13 @@
 function(){
-
 	var elem = $(this),
-		db = $$(this).app.require('db').db
-		// TODO: Get model working
-		//var model = $$(this).app.require('model').model
-		// Example usage of CouchObject model
+	config = $$(this).app.require('config'),
+	db = config.db,
+	model = $$(this).app.require('model').model,
+	CGroup = model.types.CGroup,
+	Directory = model.types.Directory,
+	Cong = model.types.Cong
+	// TODO: Get model working
+	// Example usage of CouchObject model
 //		// Create a cong
 //		var cong = model.types.Cong.init({
 //		    name:'Caney OPC',
@@ -16,6 +19,12 @@ function(){
 //		var cong = model.types.Cong.init(id)
 //		groups = cong.groups
 //		
+	var cong1 = Cong.init({
+	    name:'Caney OPC',
+	    mailing_state:'KS'
+	})
+	console.log(cong1)
+	cong1.save()
 	
 	// Delay this to run after typing has stopped for 2 seconds, so we don't send too many requests
 	// TODO: Don't fire on every key event, but only once after delay.
@@ -24,7 +33,7 @@ function(){
 
 	function populate_dir(cgroup){
 		// Populate directory with new data from page
-		if (typeof dir.cgroup_id === 'undefined') {
+		if (dir.cgroup_id === undefined) {
 			dir.cgroup_id = cgroup.id
 		}
 		// Check to see whether any directory already uses the URL the user is entering, 
@@ -33,70 +42,75 @@ function(){
 		//	the db automatically.
 		dir.url = $('#url').val();
 		dir.get_url_contents = true // triggers changes_listener.js
-		// TODO: Should we do a fuzzy search or autocomplete, to get the user to pick the right cgroup?
+		// TODO: Should we do a fuzzy search or autocomplete, to get the user to pick the 
+		//	right cgroup?
 
 		// Save request to get contents of URL to database
 		// TODO: Move the rev-checking and object syncing code below into CouchAppObject.js
 		// Get current object from db
-		db.openDoc(dir._id, {
-			success:function(doc){
-				// Save rev for after merge
-				var rev = doc._rev
-				// Merge browser's copy of dir with db copy, retaining URL from browser
-				$.extend(doc, dir)
-				// restore rev
-				doc._rev = rev
-				dir = doc
-				// Save new doc to db
-				db.saveDoc(dir, {
-					success:function(msg){
-						// Set the local copy of the directory's new _rev
-						dir._rev = msg.rev
-						// Watch for Node changes listener's response
-						var changes = db.changes();
-						changes.onChange(function(change){
-							// Determine if the changed document is the one we are editing 
-							var change_id = change.results[0].id
-							if (change_id == dir._id){
-								// Get document by id
-								db.openDoc(change_id, {
-									success:function(doc){
-										// Put new dir from db into memory
-										// TODO: This should set dir._rev to the rev in the db, but doesn't,
-										//	so it causes a doc update conflict in the browser
-										dir = doc;
-										// if the new doc has a value for url_html
-										if (dir.url_html){
-											// In the controller & output to form, handle whether this is an RSS feed or an HTML page
-											// Determine whether url_html contains HTML or RSS
-											if (dir.url_html.indexOf("</html>") > -1){
-												$("#directory_type").show(1000);
-												$("#rss_feed").hide(1000);
-												dir.pagetype = 'html';
+		console.log(dir)
+		if (dir._id){
+			// We've already gotten this dir out the db
+			db.openDoc(dir._id, {
+				success:function(doc){
+					// Save rev for after merge
+					var rev = doc._rev
+					// Merge browser's copy of dir with db copy, retaining URL from browser
+					$.extend(doc, dir)
+					// restore rev
+					doc._rev = rev
+					dir = doc
+					// Save new doc to db
+					db.saveDoc(dir, {
+						success:function(msg){
+							// Set the local copy of the directory's new _rev
+							dir._rev = msg.rev
+							// Watch for Node changes listener's response
+							var changes = db.changes();
+							changes.onChange(function(change){
+								// Determine if the changed document is the one we are editing 
+								var change_id = change.results[0].id
+								if (change_id == dir._id){
+									// Get document by id
+									db.openDoc(change_id, {
+										success:function(doc){
+											// Put new dir from db into memory
+											// TODO: This should set dir._rev to the rev in the db, but doesn't,
+											//	so it causes a doc update conflict in the browser
+											dir = doc;
+											// if the new doc has a value for url_html
+											if (dir.url_html){
+												// In the controller & output to form, handle whether this is an RSS feed or an HTML page
+												// Determine whether url_html contains HTML or RSS
+												if (dir.url_html.indexOf("</html>") > -1){
+													$("#directory_type").show(1000);
+													$("#rss_feed").hide(1000);
+													dir.pagetype = 'html';
+												}
+												else if (dir.url_html.indexOf("</rss>") > -1){
+													// TODO: Display the right form controls for an RSS page
+													$("#directory_type").hide(1000);
+													$("#rss_feed").show(1000);
+													dir.pagetype = 'rss';
+												}
+												else { // We got an error code
+													// Hide the form controls.
+													elem.trigger('hide_subform');
+												}
+												$("#url_result_div").innerHTML = dir.pagetype;
 											}
-											else if (dir.url_html.indexOf("</rss>") > -1){
-												// TODO: Display the right form controls for an RSS page
-												$("#directory_type").hide(1000);
-												$("#rss_feed").show(1000);
-												dir.pagetype = 'rss';
-											}
-											else { // We got an error code
-												// Hide the form controls.
-												elem.trigger('hide_subform');
-											}
-											$("#url_result_div").innerHTML = dir.pagetype;
 										}
-									}
-								});
-							}
-						});
-						// TODO:  I'm a little confused.  Because changes.onChange is asynchronous, do we need
-						//	changes.stop() here?
-						//changes.stop();
-					}
-				})
-			}
-		})
+									});
+								}
+							});
+							// TODO:  I'm a little confused.  Because changes.onChange is asynchronous, do we need
+							//	changes.stop() here?
+							//changes.stop();
+						}
+					})
+				}
+			})
+		}
 	}
 	
 		
@@ -113,11 +127,9 @@ function(){
 				success:function(data){
 					dir = {}
 					if (data.rows.length>1){
-						// TODO: Throw an error or handle the problem that this directory has multiple entries
+						// TODO: Throw an error or handle the problem that this directory 
+						//	has multiple entries
 						console.log("Error:  More than one copy of this directory's settings are found in the database.")
-						// TODO: When I refresh http://localhost:37470/rcl/_design/rcl/import_directory.html,
-						//	I get this error in an alert box: "The document could not be saved: Document update conflict."
-						//  I think this is because I'm not using the previous _rev in the saveDoc command.
 					}else if (data.rows.length==1){
 						// We found the right directory
 						// Populate dir object from db
@@ -141,6 +153,7 @@ function(){
 	// Query database by cgroup.abbreviation
 	// TODO: Turn this into a view in model.cgroup
 	// TODO: Run this when the abbreviation changes, rather than when the URL changes
+	console.log(db)
 	db.view('rcl/cgroup-by-abbreviation', {
 		keys:[$('#abbreviation').val()],
 		include_docs:true,
