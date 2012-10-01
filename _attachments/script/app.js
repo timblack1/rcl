7b7692db653d3dfa9bb04abef636024d88542c3b
DouglasHuston <dughuston@aol.com>
2012-10-01 18:42:34 -0500

    Began making a Backbone View.

--- _attachments/script/app.js
+++ _attachments/script/app.js
@@ -96,6 +96,55 @@ define(['model'], function(model){
         // TODO: Create Backbone views here
         // TODO: This tutorial shows how to use RequireJS with Backbone:
         //  http://backbonetutorials.com/organizing-backbone-using-modules/
+            MapView = Backbone.View.extend({
+        initialize: function(){
+        	//TODO: We are trying to get the AJAX request to work on the "on key up" event; but no luck so far. 
+        	var geocoder;
+        	var map;
+        	function initialize() {
+        		geocoder = new google.maps.Geocoder();
+        		// TODO: Center the map on the viewer's country
+        		var latlng = new google.maps.LatLng(-34.397, 150.644);
+        		var myOptions = {
+        				zoom: 8,
+        				center: latlng,
+        				mapTypeId: google.maps.MapTypeId.ROADMAP
+        		}
+        		map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+        	}
+        	initialize();
+
+        	// TODO: Migrate the code below into Evently events
+        	
+        	// Attach events to search_the_map form elements
+        	$('#search').click(function() {
+        		codeAddress();
+        		return false;
+        	});
+        	$('#search_the_map').keypress(function(event) {
+        		if (event.which==13){ // The user pressed the enter key
+        			event.preventDefault();  // Prevent the default action of submitting the form
+        			codeAddress();
+        			return false; // This might help some browsers avoid submitting the form
+        		}
+        	});
+            this.render();
+        },
+        render: function(){
+            var template = _.template( $("#map_template").html(), {} );
+            this.el.html( template );
+        },
+        events: {
+            "click input[type=button]": "doSearch"
+        },
+        doSearch: function( event ){
+            // Button clicked, you can access the element that was clicked with event.currentTarget
+            alert( "Search for " + $("#search_input").val() );
+        }
+    });
+
+    var map_view = new MapView({ el: $("#map") });
+        
         // TODO: Convert map to Backbone view following http://backbonetutorials.com/what-is-a-view/
         // TODO: Convert map template from Genshi to Underscore template syntax, and put in
         //  _attachments/index.html following http://backbonetutorials.com/what-is-a-view/
