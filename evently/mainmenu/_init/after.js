f94806073ee8086f9f4f383f81e152616d0465f6
Tim Black <tim@alwaysreformed.com>
2012-10-15 17:18:16 -0500

    Remove evently menu code, migrate menu event handlers into Backbone, configure Backbone router, Backbone history, pushState to enable SEF URLs, comment out backbone-relational example queries, start configuring CouchDB URL rewriting

--- evently/mainmenu/_init/after.js
+++
@@ -1,7 +0,0 @@
-function(){
-	// TODO: Get the menu item that is selected
-	var path = unescape(document.location.pathname).split('/')
-	var filename = path[path.length-1]
-	// Add the "active" class to the menu item the user clicked
-	$('#mainmenu a[href="' + filename + '"]').addClass('active')
-}
