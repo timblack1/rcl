0211d30ea83f0bd652ed0cfbaee99d22943831d7
Tim Black <tim@alwaysreformed.com>
2012-11-01 21:39:27 -0500

    Make model, config global so they're available in tests, create global status message object, explicitly make other globals attributes of the window object, make unit tests wait until AJAX call has returned

--- _attachments/script/config.js
+++ _attachments/script/config.js
@@ -7,7 +7,17 @@ define(
        //   https://blueprints.launchpad.net/reformedchurcheslocator/+spec/dynamically-get-db-name
        var db_name = 'rcl',
            db = $.couch.db(db_name),
-           run_jasmine_tests = true;
+           run_jasmine_tests = true,
+           // Select view to load at root URL
+           //default_view = 'find_a_church_view',
+           default_view = 'import_directory_view',
+           // After running tests,
+           //   Navigate to this page:
+           test_home_page = 'import_directory',
+           //test_home_page = 'find_a_church',
+           //   Display this in the address bar: (to facilitate manually refreshing the page by
+           //   hitting F5)
+           test_home_address = 'index.html';
 
        // Enables Mustache.js-like templating.
        _.templateSettings = {
@@ -22,7 +32,10 @@ define(
        return {
            db_name:db_name,
            db:db,
-           render_to_id:render_to_id
+           render_to_id:render_to_id,
+           test_home_page:test_home_page,
+           test_home_address:test_home_address,
+           default_view:default_view
        }
     }
 )
