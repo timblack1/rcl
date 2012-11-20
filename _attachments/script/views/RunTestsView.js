40047177b0e07cca56c3adb5418769f9de42aba7
Tim Black <tim@alwaysreformed.com>
2012-11-20 16:49:31 -0600

    Added RunTestsView.js

---
+++ _attachments/script/views/RunTestsView.js
@@ -0,0 +1,20 @@
+define(
+   [
+    '../config'
+    ], 
+    function(config){
+
+    var RunTestsView = Backbone.View.extend({
+        initialize: function(){
+            db = config.db
+            this.render()
+        },
+        render: function(){
+            var template = _.template( $('#test_template').html().replace(/script2/g, 'script'), {} );
+            $(this.el).html( template );
+        }     
+    });
+    
+    return RunTestsView
+
+});
