8dabfed6e3079a6979ba936c60ca3018900c548c
Tim Black <tim@alwaysreformed.com>
2013-04-01 17:36:40 -0500

    Converted to Backbone syntax

--- _attachments/script/views/ImportDirectory/Cong_Fields.js
+++ _attachments/script/views/ImportDirectory/Cong_Fields.js
@@ -50,7 +50,7 @@ define([
         events: {
             "focus #fields_table_mustache input[type=text]": "display_button"
         },
-        display_button:function(event) {
+        display_button: function(event) {
             // Record which field's text box was clicked in a global variable
             selected_field = $(event.target).attr('id');
             // Show this field's "No, this isn't right" button, and hide all others
@@ -58,14 +58,14 @@ define([
             $('#' + selected_field + '_button').show();
             // Create global directory.fields object if it doesn't already exist
             // TODO: directory itself doesn't exist yet, so this throws an error
-            if (typeof directory.fields === 'undefined') {
-                directory.fields = {};
+            if (typeof dir.get('fields') === 'undefined') {
+                dir.set('fields') = {};
             }
             // TODO:  This doesn't actually work yet, for some reason
-            // If this field's settings have already been stored in directory.fields, 
+            // If this field's settings have already been stored in dir.fields, 
             //      load it in the textarea
-            if (directory.fields[selected_field] !== undefined){
-                $('#details_regex').val(directory.fields[selected_field].regex);
+            if (dir.get('fields')[selected_field] !== undefined){
+                $('#details_regex').val(dir.get('fields')[selected_field].regex);
             }
             // Listen for and handle a selection event
             $('#cong_details_fields_selector').mouseup(create_regular_expression());
