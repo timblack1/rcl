e508b541618e7a93db8c447d4bc5a0383cff11a0
Tim Black <tim@alwaysreformed.com>
2013-08-27 02:37:55 -0500

    Commented out build lines that we might not want

--- _attachments/build/build.sh
+++ _attachments/build/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 r.js -o build/app.build.js
-cd ../dist
-mv js/vendor/requirejs/require.js require.js
-rm -rf js/vendor/* build
-mkdir js/vendor/requirejs && mv require.js js/vendor/requirejs/require.js
-mv css/main.css main.css && rm -rf css/*
+# cd ../dist
+# mv js/vendor/requirejs/require.js require.js
+# rm -rf js/vendor/* build
+# mkdir js/vendor/requirejs && mv require.js js/vendor/requirejs/require.js
+# mv css/main.css main.css && rm -rf css/*
