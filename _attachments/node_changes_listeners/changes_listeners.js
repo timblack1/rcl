ca291f8c8618729eda7f592f4683a37edecf6dad
Tim Black <tim@alwaysreformed.com>
2014-02-03 17:34:30 -0600

    Refactor to only process changes after update_seq, to speed up loading the changes feed

--- _attachments/node_changes_listeners/changes_listeners.js
+++ _attachments/node_changes_listeners/changes_listeners.js
@@ -113,7 +113,7 @@ function recurse_then_save(i, options){
 }
 function recurse_urls(i, options){
     if (typeof options.doc[options.from_urls] == 'undefined'){
-        console.log(options.doc[options.from_urls])
+        // console.log(options.doc[options.from_urls])
     }
     // Stop running if we have reached the end of the list of URLs,
     if (options.doc[options.from_urls][i] !== '' && typeof options.doc[options.from_urls][i] !== 'undefined' &&
@@ -165,56 +165,70 @@ function get_url_set(options){
     }
 }
 
-// Handle all changes
-process.on('message', function(doc){
-    // Watch for requests to get the contents of a URL for a church directory
-    // TODO: Check to see if the URL is valid
-    if (doc.collection == 'directory' && doc.get_url_html=='requested' && doc.url){
-        // E.g., when a user enters "opc.org/locator.html" into the church directory configuration page,
-        //  then go get the contents of that URL.
-        get_url(doc, 'url', 'url_html', 'get_url_html')
-    }
-    if (doc.collection == 'directory' && doc.get_cong_url_html=='requested' && doc.cong_url){
-        get_url(doc, 'cong_url_raw', 'cong_url_html', 'get_cong_url_html', {success:function(){
-            // Iterate state pages' HTML
-            for (var i=0; i<doc.state_url_html.length; i++){
-                // TODO: Get each cong's URL
-                var state_html = doc.state_url_html[i]
-                
-                // TODO: Get each cong page's HTML & write to database
+// -------- Main routine that handles all db changes --------
+
+// Only get changes after "update_seq"
+db.get('', function(err,doc){
+	// TODO: This throws:  TypeError: Cannot read property 'update_seq' of undefined
+    db.changes({since:doc.update_seq}).on('change', function (change) {
+        db.get(change.id, change.changes[0].rev, function(err, doc){
+            if (change.id && change.id.slice(0, '_design/'.length) !== '_design/') {
+                // This is a change to a data document
+                // Feed the new doc into the changes listeners
+                if (doc) { // Don't handle docs that have been deleted
+                    // Watch for requests to get the contents of a URL for a church directory
+					// TODO: Check to see if the URL is valid
+					if (doc.collection == 'directory' && doc.get_url_html=='requested' && doc.url){
+						// E.g., when a user enters "opc.org/locator.html" into the church directory configuration page,
+						//  then go get the contents of that URL.
+						get_url(doc, 'url', 'url_html', 'get_url_html')
+					}
+					if (doc.collection == 'directory' && doc.get_cong_url_html=='requested' && doc.cong_url){
+						get_url(doc, 'cong_url_raw', 'cong_url_html', 'get_cong_url_html', {success:function(){
+							// Iterate state pages' HTML
+							for (var i=0; i<doc.state_url_html.length; i++){
+								// TODO: Get each cong's URL
+								var state_html = doc.state_url_html[i]
+								
+								// TODO: Get each cong page's HTML & write to database
+							}
+						}})
+					}
+					// Watch for requests to get the contents of a state page URL
+					if (doc.collection == 'directory' && doc.get_state_url_html=='requested' && doc.state_url){
+						// Interpolate state names into URLs
+						var state_page_urls = []
+						// console.log('before interpolating state names into URLs')
+						for (var i=0; i<doc.state_page_values.length; i++){
+							if (doc.state_page_values[i] !== ''){
+								state_page_urls.push(doc.state_url.replace('{state_name}', doc.state_page_values[i]))
+							}
+						}
+						// console.log('about to get_url_set')
+						doc.state_page_urls = state_page_urls
+						get_url_set({
+							doc:               doc,
+							from_urls:          'state_page_urls',
+							method:             'state_url_method',
+							to_html:            'state_url_html',
+							status_flag:        'get_state_url_html',
+							number_downloaded:  'state_urls_gotten',
+							success:function(){
+							// TODO: Cleanup unnecessary doc attributes here?  Probably that should be done in
+							//  ImportDirectoryView.js instead.
+						}})
+					}
+					// Watch for requests to get the contents of a batchgeo map URL
+					if (doc.collection == 'directory' && doc.get_batchgeo_map_html=='requested' && doc.batchgeo_map_url){
+						get_url(doc, 'batchgeo_map_url', 'batchgeo_map_html', 'get_batchgeo_map_html')
+					}
+					// Watch for requests to get the contents of a JSON feed
+					if (doc.collection == 'directory' && doc.get_json=='requested' && doc.json_url){
+						get_url(doc, 'json_url', 'json', 'get_json')
+					}
+
+                }
             }
-        }})
-    }
-    // Watch for requests to get the contents of a state page URL
-    if (doc.collection == 'directory' && doc.get_state_url_html=='requested' && doc.state_url){
-        // Interpolate state names into URLs
-        var state_page_urls = []
-        console.log('before interpolating state names into URLs')
-        for (var i=0; i<doc.state_page_values.length; i++){
-            if (doc.state_page_values[i] !== ''){
-                state_page_urls.push(doc.state_url.replace('{state_name}', doc.state_page_values[i]))
-            }
-        }
-        console.log('about to get_url_set')
-        doc.state_page_urls = state_page_urls
-        get_url_set({
-            doc:               doc,
-            from_urls:          'state_page_urls',
-            method:             'state_url_method',
-            to_html:            'state_url_html',
-            status_flag:        'get_state_url_html',
-            number_downloaded:  'state_urls_gotten',
-            success:function(){
-            // TODO: Cleanup unnecessary doc attributes here?  Probably that should be done in
-            //  ImportDirectoryView.js instead.
-        }})
-    }
-    // Watch for requests to get the contents of a batchgeo map URL
-    if (doc.collection == 'directory' && doc.get_batchgeo_map_html=='requested' && doc.batchgeo_map_url){
-        get_url(doc, 'batchgeo_map_url', 'batchgeo_map_html', 'get_batchgeo_map_html')
-    }
-    // Watch for requests to get the contents of a JSON feed
-    if (doc.collection == 'directory' && doc.get_json=='requested' && doc.json_url){
-        get_url(doc, 'json_url', 'json', 'get_json')
-    }
-});
+        });
+    });
+})
