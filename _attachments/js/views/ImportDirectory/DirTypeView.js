c24563b457e5f6480921b09d4a70c95c108e7edc
Tim Black <tim@alwaysreformed.com>
2014-02-18 01:21:40 -0600

    Rname window.app.dir to this.model, and pass this.model as each appropriate view's model, to make the views better able to watch the same model for change events

--- _attachments/js/views/ImportDirectory/DirTypeView.js
+++ _attachments/js/views/ImportDirectory/DirTypeView.js
@@ -6,7 +6,7 @@ define([
         ], 
         function(config, Mustache, template, StatePageView){
     
-    var DirType = Backbone.View.extend({
+    return Backbone.View.extend({
         initialize:function(){
         },
         render: function(){
@@ -19,26 +19,11 @@ define([
         },
         radio_clicked:function(event){
             // Record the directory type
-            // We're using a recursive setTimeout because window.app.dir is not available at runtime here
-            // TODO: Start here 2014. Get window.app.dir to be defined so the execution can proceed from here.
-            //  The place to do that is first in URLView.js.
-            //  I've changed window.app.dir to be the model of the URLView.  Is URLView available in this scope?
-            //  This may be my next step.
-            function wait_for_dir(){
-                if (typeof window.app.dir === 'undefined'){
-                    console.error('window.app.dir is undefined')
-                    setTimeout(wait_for_dir, 500)
-                }else{
-                    console.log('window.app.dir is defined')
-                    window.app.dir.set('display_type', $('input:radio[name=type]:checked').val())
-                    this.state_page_view = new StatePageView({el: '#steps'})
-                    this.state_page_view.render()
-                }
-            }
-            wait_for_dir()
+            this.model.set('display_type', $('input:radio[name=type]:checked').val())
+            this.state_page_view = new StatePageView({el: '#steps', model: this.model})
+            this.state_page_view.render()
         }
 
     });
-    return DirType;
 
 });
--- _attachments/js/views/ImportDirectory/StatesOneStatePerPageView.js
+++ _attachments/js/views/ImportDirectory/StatesOneStatePerPageView.js
@@ -6,7 +6,7 @@ define([
         ], 
         function(config, Mustache, template, CongDetailsURLView){
     
-    var StatesOneStatePerPageView = Backbone.View.extend({
+    return Backbone.View.extend({
         initialize:function(){
             _.bindAll(this,'show_state_page')
         },
@@ -17,7 +17,7 @@ define([
             var thiz = this
             function sub_render(){
                 // TODO: Why is the second argument to rewrite_urls below undefined?
-                var new_html_set = config.rewrite_urls(window.app.dir.get('url'), [window.app.dir.get('url_html')], 0)
+                var new_html_set = config.rewrite_urls(thiz.model.get('url'), [thiz.model.get('url_html')], 0)
                 thiz.$('#state_drop_down_selector').html(new_html_set[0]);
                 // We bind the event here because the select element didn't exist at this Backbone view's
                 //  initialization
@@ -26,9 +26,9 @@ define([
             }
             // We (sometimes?) have to wait for url_html to be available
             function fetch(){
-                if (typeof window.app.dir.get('url_html') === 'undefined'){
+                if (typeof thiz.model.get('url_html') === 'undefined'){
                     setTimeout(function(){
-                        window.app.dir.fetch({success:function(model,response,options){
+                        thiz.model.fetch({success:function(model,response,options){
                             fetch()
                         }})
                     },300)
@@ -68,10 +68,10 @@ define([
             var form = el.closest('form')
             // Handle cases where there is or is not a final slash in base_url, or
             //  an initial slash in form.attr('action').
-            if(typeof window.app.dir.get('url') === 'undefined'){
-                console.error('window.app.dir.get(url) is ' + window.app.dir.get('url'))
+            if(typeof this.model.get('url') === 'undefined'){
+                console.error('this.model.get(url) is ' + this.model.get('url'))
             }
-            var base_url = window.app.dir.get('url').replace(/\/+$/,'')
+            var base_url = this.model.get('url').replace(/\/+$/,'')
             var state_url = ''
             if (form.attr('action').indexOf('/') === 0){
                 // action is a partial absolute URL, so attach it to the domain name
@@ -146,10 +146,9 @@ define([
                     }
                 }})
             }
-            save_dir(window.app.dir)
+            save_dir(this.model)
         }
 
     });
-    return StatesOneStatePerPageView;
 
 });
