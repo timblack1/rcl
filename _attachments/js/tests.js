8bd39b23dc4bd16bbdac380a17093bfb68de6eec
Tim Black <tim@alwaysreformed.com>
2014-03-10 23:32:08 -0500

    Changed test timeout

--- _attachments/js/tests.js
+++ _attachments/js/tests.js
@@ -1,7 +1,7 @@
 describe("Reformed Churches Locator", function() {
     
     // Set this to increase or decrease the tests' timeout
-    var timeout = 80000
+    var timeout = 4000
     
     afterEach(function() {
         // Remove the docs we just created
