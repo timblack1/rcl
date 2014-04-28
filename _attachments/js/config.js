296985424a0b4645b8efcc29dafffaf4622404e0
Tim Black <tim@alwaysreformed.com>
2014-04-28 18:45:37 -0500

    Prevent 409 conflict

--- _attachments/js/config.js
+++ _attachments/js/config.js
@@ -10,7 +10,7 @@ define(
        // TODO: Run unit tests on test copy of the database
        var db_name = window.location.pathname.split('/')[1],
            db = $.couch.db(db_name),
-           run_jasmine_tests = false,
+           run_jasmine_tests = true,
            // Select view to load at root URL
            //default_view = 'find_a_church_view',
            default_view = 'import_directory_view',
