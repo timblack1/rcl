8f6c9bec615ad0877b2c906da4559f714e44ad75
Tim Black <tim@alwaysreformed.com>
2014-06-25 23:46:58 -0500

    Reorganized directories to work as a Hoodie app

--- autopush.sh
+++ autopush.sh
@@ -3,23 +3,8 @@
 # This file is for you, the developer, to run while developing the code.  It automatically pushes
 #   your new changes into couchdb whenever you save a file.
 
-# . ../bin/activate
-
 MODE="dev-local"
-#MODE="dev-iriscouch"
-#MODE="prod-local"
-#MODE="prod-iriscouch"
-
-if [ "$MODE" = 'dev-iriscouch' ];
-then
-  # # Development copy - arwd.iriscouch.com
-  # # Get the admin username and password for couchdb
-  LOGIN=$(cat login.txt)
-  PORT=80
-  HOST=arwd.iriscouch.com
-  # HOST=arwd.cloudant.com
-  DBNAME=rcl-dev;
-fi
+#MODE="prod-webfaction"
 
 if [ "$MODE" = 'dev-local' ];
 then
@@ -32,23 +17,13 @@ then
   DBNAME=rcl-dev;
 fi;
 
-if [ "$MODE" = 'prod-local' ];
-then
-  # Production copy
-  # Get the admin username and password for couchdb
-  LOGIN=$(cat login.txt)
-  PORT=5984
-  HOST=localhost
-  DBNAME=rcl;
-fi;
-
-if [ "$MODE" = 'prod-iriscouch' ];
+if [ "$MODE" = 'prod-webfaction' ];
 then
   # Production copy
   # Get the admin username and password for couchdb
   LOGIN=$(cat login.txt)
   PORT=80
-  HOST=arwd.iriscouch.com
+  HOST=rcl.alwaysreformed.com/_api
   DBNAME=rcl;
 fi;
 
@@ -63,43 +38,9 @@ then
     sed -i "s/5984/$PORT/g" .couchapprc
 fi
 
-# Push app into database in case this has not been done yet
+# Push app's db functions (views, updates, etc.) into database in case this has not been done yet
 erica push $URL
 # couchapp push $URL
-
-#echo "Starting the Node.js changes listener as a forked child process..."
-#( ./_attachments/node_changes_listeners/start.sh & )
-echo "Starting the Node.js changes listener as a background process..."
-# It's in the background in order to allow iwatch to push new changes to the db
-nodejs ./_attachments/node_changes_listeners/changes_listeners.js &
-
-# You can run node-inspector like this:  node-inspector &
-
-# TODO: If I start the node script from this file too, and run autopush in the background, 
-#   I can use this example to kill the autopush process before killing this process:
-#   http://hacktux.com/bash/control/c
-# TODO: Here is how to start the forked process and kill it afterward:  http://hacktux.com/bash/ampersand
-# Kill forked process
-cleanup(){
-  # TODO: Kill the Node.js process when killing this file's process.  This kill command doesn't work.
-  #     It seems the problem is that $! is not recognized as the PID
-  kill $!
-  return $?
-}
-
-# run if user hits control-c
-control_c(){
-  printf "\n*** Exiting ***\n\n"
-  cleanup
-  exit $?
-}
-
-# trap keyboard interrupt (control-c)
-trap control_c INT
-
-# Launch the application in the browser
-# erica browse rcl
-xdg-open $URL/_design/rcl/index.html &
 
 # Start watching the filesystem for changes, and push new changes into the database
 # Avoid multiple pushes caused by text editors saving the file more than once.
