78e75475ad5ad74583974809efb5cd5aa308cc03
Tim Black <tim@alwaysreformed.com>
2014-09-23 01:00:40 -0500

    Updated TODOs

--- www/assets/js/views/FindAChurch/MapView.js
+++ www/assets/js/views/FindAChurch/MapView.js
@@ -75,7 +75,7 @@ define([
                 var north_lat = north_east.lat();
                 var south_lat = south_west.lat();
     
-                // TODO: Modify this to search congs in hoodie's local store
+                // TODO: Modify this to filter congs in hoodie's local store by the specified bounds
                 // Send AJAX call to geocouch containing bounds within which congregations are found
                 // Geocouch uses GeoJSON coordinates, which are lower left, then upper right, which is the same
                 //  order Google Maps uses
