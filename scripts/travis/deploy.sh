89999e62978889b140074c32e4ced1804dd059ca
Tim Black <tim@alwaysreformed.com>
2015-10-13 18:04:11 -0500

    fix: fix remote deploy URL

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -13,8 +13,8 @@ chmod 600 ~/.ssh/id_dsa # this key should have push access
 ssh-add ~/.ssh/id_dsa
 # Add SSH key fingerprint to known_hosts
 echo "|1|oHsg/KauDxCV9IrVipFmiQ8ZtQE=|NX0LlPZE1+MO6Sxf9SlGb6fHMmI= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
-git remote add deploy ssh://timblack1@timblack1.webfactional.com:webapps/rcl
-git push deploy
+git remote add deploy timblack1@timblack1.webfactional.com:webapps/rcl
+git push deploy master
 
 # Update npm dependencies on production server
 
