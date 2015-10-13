3a5f0d113d882d178ac93e1e09f2c57bdbfa98a6
Tim Black <tim@alwaysreformed.com>
2015-10-13 18:26:56 -0500

    fix: update key by adding both DSA and RSA keys

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -11,8 +11,14 @@ git commit -am "add dist directory"
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 ~/.ssh/id_dsa # this key should have push access
 ssh-add ~/.ssh/id_dsa
+chmod 600 ~/.ssh/id_rsa # this key should have push access
+ssh-add ~/.ssh/id_rsa
 # Add SSH key fingerprint to known_hosts
 echo "|1|oHsg/KauDxCV9IrVipFmiQ8ZtQE=|NX0LlPZE1+MO6Sxf9SlGb6fHMmI= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
+echo "|1|h9vGsRLcjfPVOaFDemRRFrBTQhU=|bhvBkO38uMeNwfHAsu7zG1Nr3js= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
+echo "|1|V44YyF7R1eFyn+mxJBlmP4neB/8=|S1izmqdEdeCix9AoTU5CQVpeLRI= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
+echo "|1|2WS094AuKUEKrVFgmQQhlGi/npc=|hK7oz6AY/WYcLwb6whngTACng5w= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
+echo "|1|WCA7QffbfOse2kmkNFxPfBYsFzo=|7doD/PASp/PX+Gu4j3KebXlVsww= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAx7Td272cgnoVeBPINklRhV8EwAQDHrUEcLMMshWwhg7bANN889gaZVvLzKYlKTasJ1qeF7Gq+J+d4Kg2aVoP++EmvCid/5ukDYxDJHPIojYHp6iY1S1KhhgMlfgWWY5Tk3CeItby7lQHQXfTXaunKN3OJKRcIOaJQl9fiKsDJ6k=" >> ~/.ssh/known_hosts
 git remote add deploy timblack1@timblack1.webfactional.com:webapps/rcl
 git push deploy master
 
