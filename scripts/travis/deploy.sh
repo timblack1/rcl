125062b9c12aac43c347335cf6c20c9d30f847b4
Tim Black <tim@alwaysreformed.com>
2015-10-20 00:19:23 -0500

    Fixing gulp path and logging stages

--- scripts/travis/deploy.sh
+++ scripts/travis/deploy.sh
@@ -2,7 +2,7 @@
 
 # Run gulp task to prep for deployment
 
-gulp default
+node node_modules/gulp/bin/gulp.js default
 git add .
 git commit -am "add dist directory"
 
@@ -20,10 +20,12 @@ echo "|1|V44YyF7R1eFyn+mxJBlmP4neB/8=|S1izmqdEdeCix9AoTU5CQVpeLRI= ssh-rsa AAAAB
 echo "|1|2WS094AuKUEKrVFgmQQhlGi/npc=|hK7oz6AY/WYcLwb6whngTACng5w= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA3V+waEkDMazM7oWm3dpqr8YXMUD86NJgcOl2N9UkPQmozqnHVvrFhABoEwnFb3oreTRrXG/NTyevpvs7eOwWzXkBvGHCwcr70CISWM3do9KreQBBKoYFXW5fUe2/z2wYwrLydPMsnKUBtyiSggsOdWRBnJYI4M0Wdh49TnhNktVbV+i2N/FQnGUNTm/YwI3Lykjy7qMIE8WUMeifpHh3md8c51WK8gzIqsjej614uWFn4q0LqRx2QhpvLKHVicUfACzkF3GfHQ9xlhdCgWqYoaI6ECS9JeZKKFOXIgfidBWZqKniQUrQFgNL2dLIWROE8yFEpxUUhQyfLI4wzqFiow==" >> ~/.ssh/known_hosts
 echo "|1|WCA7QffbfOse2kmkNFxPfBYsFzo=|7doD/PASp/PX+Gu4j3KebXlVsww= ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAx7Td272cgnoVeBPINklRhV8EwAQDHrUEcLMMshWwhg7bANN889gaZVvLzKYlKTasJ1qeF7Gq+J+d4Kg2aVoP++EmvCid/5ukDYxDJHPIojYHp6iY1S1KhhgMlfgWWY5Tk3CeItby7lQHQXfTXaunKN3OJKRcIOaJQl9fiKsDJ6k=" >> ~/.ssh/known_hosts
 git remote add deploy timblack1@timblack1.webfactional.com:webapps/rcl
+echo "Pushing to master on production server..."
 git push deploy master
 
 # Update npm dependencies on production server
 
+echo "Updating npm dependencies on production server..."
 ssh timblack1@timblack1.webfactional.com 'cd webapps/rcl && npm update'
 
 # Restart app on production server
