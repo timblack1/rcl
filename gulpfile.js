83c29e647fe83add6c4cf4c255f68625069857b3
Tim Black <tim@alwaysreformed.com>
2015-12-16 23:06:54 -0600

    Update from gulp-usref v2 to v3 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -74,14 +74,16 @@ var imageOptimizeTask = function(src, dest) {
 };
 
 var optimizeHtmlTask = function(src, dest) {
-//   var assets = $.useref.assets({
-//     searchPath: ['.tmp', 'app', dist()]
-//   });
+  var assets = $.useref.assets({
+    searchPath: ['.tmp', 'app', dist()]
+  });
 
   return gulp.src(src)
     // Replace path for vulcanized assets
     .pipe($.if('*.html', $.replace('elements/elements.html', 'elements/elements.vulcanized.html')))
-    //.pipe(assets)
+    .pipe(useref({
+      searchPath: ['.tmp', 'app', dist()]
+    }))
     // Concatenate and minify JavaScript
     .pipe($.if('*.js', $.uglify({
       preserveComments: 'some'
@@ -90,7 +92,7 @@ var optimizeHtmlTask = function(src, dest) {
     // In case you are still using useref build blocks
     .pipe($.if('*.css', $.minifyCss()))
     //.pipe(assets.restore())
-    .pipe($.useref())
+    //.pipe($.useref())
     // Minify any HTML
     .pipe($.if('*.html', $.minifyHtml({
       quotes: true,
