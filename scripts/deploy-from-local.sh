28086475508876691a1336bd328d095707c4fdea
Tim Black <tim@alwaysreformed.com>
2015-12-18 15:45:15 -0600

    Revert "add dist directory"
    This reverts commit adaf8104ce392564611208d93b90ec6ce3717f0f.

--- scripts/deploy-from-local.sh
+++ scripts/deploy-from-local.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Usage: This script is intended to be run from the root rcl directory, like this:
-# ./scripts/deploy-from-local.sh
-
 git checkout production
 git merge master
 
 # Run gulp task to prep for deployment, and commit dist directory to git
 
-# Usually these commands won't be needed, so I've commented them out to make the 
-#   script run faster.
-# They can be run manually when the deploy process fails.
-
-# rm -rf node_modules
-# npm install
-
 echo "Running gulp default to create dist directory..."
 gulp default
 echo "Removing dist from .gitignore..."
-sed -i '/dist/d' ./.gitignore
+sed -i.bak '/dist/d' ./.gitignore
 
 git add dist
 git commit -am "add dist directory"
