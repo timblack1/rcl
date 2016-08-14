2820d7821bbe4763eca0baa1d1905f9abb97b48a
Tim Black <tim@alwaysreformed.com>
2016-08-13 23:01:33 -0500

    fix(deploy): remove Jan's daemonization since we're using pm2 to manage processes

--- scripts/pm2/rcl_start.sh
+++ scripts/pm2/rcl_start.sh
@@ -1,26 +1,12 @@
 #!/bin/sh -e
 
-# start:
+# This file does the following:
 # call node_modules/hoodie-server/bin/start manually
 # pipe stdout & stderr into files
-# send to bg
-# capture pid
-# write pid to pidfile
-
-# stop
-# read pidfile
-# if no pidfile,
-#   say no process
-# kill -INT pid # so couchdb can stop gracefully
-# rm the pidfile
 
 userhome=/home/timblack1
 apphome=$userhome/webapps/rcl
 pidfile=$apphome/data/hoodie.pid
-# Note that we put the logs here because Webfaction runs
-# logrotate on ~/logs/user/.  For this reason, we also have
-# made symbolic links from ~/webapps/rcl/data/couch.stderr
-# and ~/webapps/rcl/data/couch.stdout to ~/logs/user/.
 stdoutfile=$userhome/logs/user/rcl.stdout
 stderrfile=$userhome/logs/user/rcl.stderr
 #hoodie_user=hoodie
@@ -30,54 +16,23 @@ stderrfile=$userhome/logs/user/rcl.stderr
 
 cd $apphome
 
-case "$1" in
-  start)
-    # if pidfile exists, report and exit
-    # expect user to clean up stale pidfile
-    if [ -f "$pidfile" ]; then
-      echo "Pidfile still exists: $pidfile:"
-      cat $pidfile
-      exit 2
-    fi
+# ---------------------------------------------------------------------
+# This block is the version of this file from before we integrated code
+# from Jan's hoodie-daemon.sh.
+#export HOODIE_BIND_ADDRESS=0.0.0.0:11411
+#export COUCH_URL=http://127.0.0.1:11411
+# hoodie start -n --custom-ports 20188,19911,27069 -w dist
+# TODO: Decide whether the following is what we need instead of the above
+# gulp serve:dist
+# ---------------------------------------------------------------------
 
-    # ---------------------------------------------------------------------
-    # This block is the version of this file from before we integrated code
-    # from Jan's hoodie-daemon.sh.
-    #export HOODIE_BIND_ADDRESS=0.0.0.0:11411
-    #export COUCH_URL=http://127.0.0.1:11411
-    # hoodie start -n --custom-ports 20188,19911,27069 -w dist
-    # TODO: Decide whether the following is what we need instead of the above
-    # gulp serve:dist
-    # ---------------------------------------------------------------------
+# the command
+# sudo -u $hoodie_user \
+# COUCH_URL=http://127.0.0.1:6003 \
+HOODIE_ADMIN_USER=admin \
+HOODIE_ADMIN_PASS="$HOODIE_ADMIN_PASS" \
+HOME=$apphome \
+node node_modules/hoodie-server/bin/start --custom-ports 20188,19911,27069 -w dist \
+1>>$stdoutfile \
+2>>$stderrfile \
 
-    # the command
-    # sudo -u $hoodie_user \
-    # COUCH_URL=http://127.0.0.1:6003 \
-    HOODIE_ADMIN_USER=admin \
-    HOODIE_ADMIN_PASS="$HOODIE_ADMIN_PASS" \
-    HOME=$apphome \
-    node node_modules/hoodie-server/bin/start --custom-ports 20188,19911,27069 -w dist \
-    1>>$stdoutfile \
-    2>>$stderrfile \
-    &
-
-    pid=$!
-    echo $pid > $pidfile
-    echo "Started."
-  ;;
-
-  stop)
-    if [ ! -f "$pidfile" ]; then
-      echo "Pidfile $pidfile does not exist."
-      exit 3
-    fi
-    pid=$(<$pidfile)
-    kill -INT $pid
-    rm $pidfile
-    echo "Stopped."
-  ;;
-
-  *)
-    echo "Invalid or missing command. Try 'start' or 'stop'."
-  ;;
-esac
